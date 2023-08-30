134376571740963d06c192cc9cf799c61262552b
Nika Oniani <nikaoniani2018@gmail.com>
2023-08-30 22:26:17 +0400

    created a cube and it moves with each click

--- src/components/Cube/Cube.js
+++ src/components/Cube/Cube.js
@@ -1,21 +1,33 @@
-import React from "react";
-import { createRoot } from "@react-three/fiber";
+import React, { useRef } from "react";
+import { createRoot, useFrame } from "@react-three/fiber";
 import { Canvas } from "@react-three/fiber";
 
 import classes from "./Cube.module.css";
 
 const Cube = () => {
+  const cubeRef = useRef();
+
+  const cubeMove = () => {
+    cubeRef.current.rotation.x += 6;
+    cubeRef.current.rotation.y += 3;
+    cubeRef.current.rotation.z += 1;
+  }
+
   return (
-    <div id="canvas-container"
-    style={{
-      width: "100vw",
-      height: "100vh"
-    }}
+    <div
+      id="canvas-container"
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
     >
-      <Canvas 
-      style={{background: "lightgrey"}}
-      >
-          <boxGeometry />
+      <Canvas onClick={cubeMove} style={{ background: "black" }}>
+        <ambientLight intensity={0.5} color="white" />
+        <pointLight position={[1, 1, 1]} />
+        <mesh ref={cubeRef}>
+          <boxGeometry position={[1, 1, 1]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
       </Canvas>
     </div>
   );
